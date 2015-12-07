3cc1df2efa7acff890b55c24cc9219eff3b69a65
elizabit <eafink@live.unc.edu>
2015-12-07 16:11:09 -0500

    Update server.R

--- server.R
+++ server.R
@@ -15,11 +15,14 @@ wb.data = WDI( country = c('AU', 'AF', 'BE', 'BZ', 'BR', 'CM', 'CA', 'CL', 'CN',
 names(wb.data) = c("remove", "Country.Name", "Year","Life.E", "Imm.Meas", "Imm.DPT", "Pub.edu", "Lit.R")
 wb.data = wb.data[ , 2:8]
 
+
+#data for ggplots
 wb.data2 = WDI( country = c('AU', 'AF', 'BE', 'BZ', 'BR', 'CM', 'CA', 'CL', 'CN', 'CO', 'DK', 'EG', 'FI', 'FR', 'DE', 'GR', 'GL', 'IS', 'IN', 'IR', 'IQ', 'IE', 'IT', 'JP', 'KE', 'ML', 'MX', 'MA', 'NZ', 'NG', 'RU', 'SA', 'ZA', 'ES', 'TR', 'UY', 'US', 'GB', 'AR'), indicator = c("SE.ADT.LITR.ZS", "SP.DYN.LE00.IN", "SE.XPD.TOTL.GD.ZS", "SH.IMM.MEAS", "SH.IMM.IDPT"), start = 1994, end = 2014, extra = T)
 wb.data2 = wb.data2[ , c(2:8, 10, 14)]
 names(wb.data2) = c("Country.Name", "Year", "Lit.R", "Life.E", "Pub.edu", "Imm.Meas", "Imm.DPT", "Region", "Income")
 
-
+wb.data3 = wb.data
+wb.data3 = wb.data3[order(wb.data3$Year, decreasing = FALSE), ]
 
 function(input, output) {
   set.seed(1234)
@@ -49,12 +52,12 @@ function(input, output) {
   })
   output$plot4 <- renderPlot({
     plot4shiny = ggplot(data = wb.data2, aes(x = Imm.DPT, y = Life.E))
-    plot4shiny + geom_point(aes(alpha = .5, color = Region, size = .1)) + guides(alpha = F, size = F) + xlab("DPT Immunization") + ylab("Life Expectancy (years)") + ggtitle("DPT Immunization and Life Expectancy")
+    plot4shiny + geom_point(aes(alpha = .5, color = Region, size = .1)) + guides(alpha = F, size = F) + xlab("DPT Immunization (% of Population)") + ylab("Life Expectancy (years)") + ggtitle("DPT Immunization and Life Expectancy")
     
   })
   output$plot5 <- renderPlot({
     plot5shiny = ggplot(data = wb.data2, aes(x = Imm.Meas, y = Life.E))
-    plot5shiny + geom_point(aes(alpha = .5, color = Region, size = .1)) + guides(alpha = F, size = F) + xlab("DPT Immunization") + ylab("Life Expectancy (years)") + ggtitle("DPT Immunization and Life Expectancy")
+    plot5shiny + geom_point(aes(alpha = .5, color = Region, size = .1)) + guides(alpha = F, size = F) + xlab("DPT Immunization (% of Population)") + ylab("Life Expectancy (years)") + ggtitle("DPT Immunization and Life Expectancy")
     
   })
   output$plot6 <- renderPlot({
@@ -62,5 +65,10 @@ function(input, output) {
     plot6shiny + geom_point(aes(alpha = .5, color = Region, size = .1)) + guides(alpha = F, size = F) + xlab("Income Level Grouping") + ylab("Life Expectancy (years)") + ggtitle("Income and Life Expectancy")
     
   })
-  output$table1 <- renderTable(wb.data)
+  output$table1 <- renderTable(wb.data3[1:156, ])
+  output$table2 <- renderTable(wb.data3[157:312, ])
+  output$table3 <- renderTable(wb.data3[313:468, ])
+  output$table4 <- renderTable(wb.data3[469:624, ])
+  output$table5 <- renderTable(wb.data3[625:780, ])
+  output$table6 <- renderTable(wb.data3[781:819, ])
 }
